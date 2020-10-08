323ad47e4ea7352f5e333e5028a8bff1df4a6882
Nico Rithner <nicorithner@gmail.com>
2020-10-08 10:55:50 -0600

    Add VCR config

--- spec/features/user_can_search_by_state_spec.rb
+++ spec/features/user_can_search_by_state_spec.rb
@@ -1,43 +1,42 @@
 require 'rails_helper'
 
 
-feature "user can search for house members" do
+describe 'user can search for house members' do
+  it 'user submits valid state name', :vcr do
 
-  scenario "user submits valid state name" do
+    # json_response = File.read('spec/fixtures/members_of_the_house.json')
 
-    json_response = File.read('spec/fixtures/members_of_the_house.json')
+    # stub_request(:get, "https://api.propublica.org/congress/v1/members/house/CO/current.json").
+    #   with( headers: { 'X-Api-Key'=> ENV["PROPUBLICA_API_KEY"] }).to_return(status: 200, body: json_response)
 
-    stub_request(:get, "https://api.propublica.org/congress/v1/members/house/CO/current.json").
-      with( headers: { 'X-Api-Key'=> ENV["PROPUBLICA_API_KEY"] }).to_return(status: 200, body: json_response)
+    # VCR.use_cassette( 'propublica_members_for_colorado' ) do
 
-    # As a user
-    # When I visit "/"
-    visit '/'
+      visit root_path
 
-    select "Colorado", from: :state
-    click_on "Locate Members of the House"
+      select "Colorado", from: :state
+      click_on "Locate Members of the House"
 
-    expect(current_path).to eq(search_path)
-    expect(page).to have_content("7 Results")
-    expect(page).to have_css(".member", count: 7)
+      expect(current_path).to eq(search_path)
+      expect(page).to have_content("7 Results")
+      expect(page).to have_css(".member", count: 7)
 
-    within(first(".member")) do
-      expect(page).to have_css(".name")
-      name = find(".name").text
-      expect(name).to_not be_empty
+      within(first(".member")) do
+        expect(page).to have_css(".name")
+        name = find(".name").text
+        expect(name).to_not be_empty
 
-      expect(page).to have_css(".role")
-      role = find(".role").text
-      expect(role).to_not be_empty
+        expect(page).to have_css(".role")
+        role = find(".role").text
+        expect(role).to_not be_empty
 
-      expect(page).to have_css(".party")
-      party = find(".party").text
-      expect(party).to_not be_empty
+        expect(page).to have_css(".party")
+        party = find(".party").text
+        expect(party).to_not be_empty
 
-      expect(page).to have_css(".district")
-      district = find(".district").text
-      expect(district).to_not be_empty
-    end
-
+        expect(page).to have_css(".district")
+        district = find(".district").text
+        expect(district).to_not be_empty
+      end
+    # end
   end
 end
